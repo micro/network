08335e15e406b56d9d717ab3a11141d443eaa99b
Asim Aslam <asim@aslam.me>
2022-05-31 08:37:31 +0100

    .

--- mucp/default.go
+++ mucp/default.go
@@ -90,6 +90,32 @@ type message struct {
 	session tunnel.Session
 }
 
+// routeToProto encodes route into protobuf and returns it
+func routeToProto(route router.Route) *pbNet.Route {
+	return &pbNet.Route{
+		Service: route.Service,
+		Address: route.Address,
+		Gateway: route.Gateway,
+		Network: route.Network,
+		Router:  route.Router,
+		Link:    route.Link,
+		Metric:  int64(route.Metric),
+	}
+}
+
+// protoToRoute decodes protobuf route into router route and returns it
+func protoToRoute(route *pbNet.Route) router.Route {
+	return router.Route{
+		Service: route.Service,
+		Address: route.Address,
+		Gateway: route.Gateway,
+		Network: route.Network,
+		Router:  route.Router,
+		Link:    route.Link,
+		Metric:  route.Metric,
+	}
+}
+
 // newNetwork returns a new network node
 func newNetwork(opts ...Option) Network {
 	// create default options
