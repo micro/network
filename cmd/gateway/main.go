6b5f1fd248127c5f411bb7e8869660c55e1116d4
asim <asim@aslam.me>
2023-05-24 11:50:15 +0100

    refactor out handler into gateway

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,10 +2,51 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/micro/network"
 )
+
+func handler() http.Handler {
+	// Parse the target URL
+	apiURL, err := url.Parse("http://localhost:8080")
+	if err != nil {
+		log.Fatal("Failed to parse api URL: ", err)
+	}
+
+	// Create the api proxy
+	apiProxy := httputil.NewSingleHostReverseProxy(apiURL)
+
+	// Parse the target URL
+	webURL, err := url.Parse("http://localhost:8082")
+	if err != nil {
+		log.Fatal("Failed to parse web URL: ", err)
+	}
+
+	// Create the web proxy
+	webProxy := httputil.NewSingleHostReverseProxy(webURL)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// serve the api
+		if strings.HasPrefix(r.URL.Path, "/api") {
+			r.URL.Path = strings.TrimPrefix(r.URL.Path, "/api")
+			apiProxy.ServeHTTP(w, r)
+			return
+		}
+
+		// set the header
+		r.Header.Set("Micro-API", "/api")
+
+		if strings.HasPrefix(r.URL.Path, "/") {
+			// serve the web proxy
+			webProxy.ServeHTTP(w, r)
+		}
+	})
+}
 
 func main() {
 	token := os.Getenv("MICRO_NETWORK_TOKEN")
@@ -15,11 +56,12 @@ func main() {
 
 	net := network.New(
 		network.WithName("gateway"),
+		network.WithHandler(handler()),
 		network.WithToken(token),
 	)
 
 	if err := net.Connect(); err != nil {
-		log.Fatal(err)
+		log.Fatal("Connect error: ", err)
 	}
 	defer net.Close()
 }
